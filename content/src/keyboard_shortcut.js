4ef303a3fb1aa4c772ab643e5a71b7e8f66a58e8
Gaëtan Vanden Bergh <55689427+GaetanVandenBergh@users.noreply.github.com>
2023-10-25 21:20:32 +0200

    [IMP] Keyboard Shortcut

--- content/src/keyboard_shortcut.js
+++ content/src/keyboard_shortcut.js
@@ -1,9 +1,28 @@
+const defaultShortcut = {
+    kbsEnabled: true,
+    kbsAltKey: true,
+    kbsCtrlKey: true,
+    //kbsMetaKey: false, Mac OS
+    kbsShiftKey: false,
+    kbsCodeKey: 'Period',
+    kbsDisplayKey: 'Period (:)'
+};
+
 async function updateTabState(url) {
     document.onkeyup = async (e) => {
-        if (e.ctrlKey && e.altKey && e.key === ':') {
-            if (confirm('The current tab will be reload.\nDo you confirm your action?')) {
-                sendUpdate();
-            }
+        const shortcut = await chrome.storage.sync.get(defaultShortcut);
+
+        if (!shortcut.kbsEnabled) return;
+        if (shortcut.kbsAltKey && !e.altKey) return;
+        if (shortcut.kbsCtrlKey && !e.ctrlKey) return;
+        //if (shortcut.kbsMetaKey && !e.metaKey) return;
+        if (shortcut.kbsShiftKey && !e.shiftKey) return;
+        console.log(e.code)
+        console.log(shortcut.kbsCodeKey)
+        if (shortcut.kbsCodeKey !== e.code) return;
+
+        if (confirm('The current tab will be reload.\nDo you confirm your action?')) {
+            sendUpdate();
         }
     };
 
