7a7f45b6dafc67c9a8f56f631da8b73d6a1f916f
Gaetan Vanden Bergh (gavb) <gavb@odoo.com>
2024-01-03 15:57:21 +0100

    [IMP] Default server action type

---
+++ content/src/new_server_action_code.js
@@ -0,0 +1,27 @@
+function checkNewServerActionPage(currentUrl) {
+    const url = new URL(currentUrl);
+    if (url.searchParams.has('id')) return;
+
+    const hash = url.hash;
+    const idRegex = /([#&])(id=\d+)/;
+    if (idRegex.test(hash)) return;
+    if (!hash.includes('model=ir.actions.server')) return;
+    if (!hash.includes('view_type=form')) return;
+
+    selectExecuteCode();
+}
+
+function selectExecuteCode() {
+    const badge = document.querySelector(`span.o_selection_badge[value='"code"']`);
+    if (badge) {
+        // Odoo 16.4+
+        badge.click();
+        return;
+    }
+    // Odoo < 16.4
+    const codeOption = document.querySelector(`option[value='"code"']`);
+    if (!codeOption) return;
+    const selector = codeOption.parentElement;
+    selector.value = codeOption.value;
+    selector.dispatchEvent(new Event('change'));
+}
