ce5a8d5525edac13df175e5bd9f8e141c7a0b8e3
Gaetan Vanden Bergh (gavb) <gavb@odoo.com>
2024-01-12 14:13:16 +0100

    [ADD] New feature & Styling

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('copyright-year').innerHTML = new Date().getFullYear();
+
+    if (new URL(location.href).searchParams.get('odoo') != null) {
+        document.getElementsByTagName('body')[0].classList.add('odoo-style');
+    }
 });
