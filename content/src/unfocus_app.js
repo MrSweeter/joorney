53cd82ea6b0b2d735dd8ba46eb607ed6a6d864cf
Gaëtan Vanden Bergh <55689427+GaetanVandenBergh@users.noreply.github.com>
2024-01-12 10:48:48 +0100

    [IMP] Superfocus apps (#14)

--- content/src/unfocus_app.js
+++ content/src/unfocus_app.js
@@ -1,3 +1,10 @@
+const UNFOCUS_STATE = {
+    UNFOCUS: 0,
+    DEFAULT: 1,
+    SUPER: 2,
+};
+UNFOCUS_STATE.MAX = UNFOCUS_STATE.SUPER;
+
 const FOCUS_OPACITY = 1;
 const UNFOCUS_OPACITY = 0.1;
 const STAR_ELEMENT_CLASS = 'qol-focus-app';
@@ -5,11 +12,15 @@ const FOCUS_ICON = 'fa-star';
 const UNFOCUS_ICON = 'fa-star-o';
 const SHARED_ORIGIN = 'unfocus_app_shared';
 
+let superfocusImageURL = '';
+
 async function appendUnfocusApp(url, count = 0) {
     const configuration = await chrome.storage.sync.get({
         unfocusAppEnabled: false,
         unfocusAppReorderEnabled: false,
         unfocusAppShareEnabled: false,
+        unfocusAppLightImageURL: '',
+        unfocusAppDarkImageURL: '',
         unfocusAppOrigins: {},
     });
     if (!configuration.unfocusAppEnabled) return;
@@ -19,6 +30,11 @@ async function appendUnfocusApp(url, count = 0) {
     if (!authorizedFeature) return;
 
     if (window.location.origin !== origin) return;
+
+    superfocusImageURL =
+        (await getThemeModeCookie(origin)) === 'light'
+            ? configuration.unfocusAppLightImageURL
+            : configuration.unfocusAppDarkImageURL;
 
     const elements = document.getElementsByClassName('o_app');
     if (elements.length == 0 && count == 0) {
@@ -36,16 +52,19 @@ async function appendUnfocusApp(url, count = 0) {
     // Append "star" before app name
     for (let element of elements) {
         const app = element.getAttribute('data-menu-xmlid');
-        const isUnfocus = unfocusAppList[app];
-        element.style.opacity = isUnfocus ? UNFOCUS_OPACITY : FOCUS_OPACITY;
+        let state = unfocusAppList[app];
+        if (state === undefined) state = UNFOCUS_STATE.DEFAULT;
+        if (state === true) state = UNFOCUS_STATE.UNFOCUS;
+        updateAppElement(element, state);
 
         const divElement = element.getElementsByClassName('o_caption')[0];
         for (let star of divElement.getElementsByClassName(STAR_ELEMENT_CLASS)) {
             star.remove();
         }
 
+        const isUnfocus = state === UNFOCUS_STATE.UNFOCUS;
         divElement.innerHTML = `<i class="qol-focus-app fa
-            ${isUnfocus ? UNFOCUS_ICON : FOCUS_ICON} me-1">
+            ${isUnfocus ? UNFOCUS_ICON : FOCUS_ICON} me-1" data-qol-state="${state}">
         </i>${divElement.innerHTML}`.trim();
     }
 
@@ -53,24 +72,47 @@ async function appendUnfocusApp(url, count = 0) {
 
     // Manage "star" click
     const apps = Array.from(document.getElementsByClassName(STAR_ELEMENT_CLASS));
+    const defaultApps = [];
+    const unfocusApps = [];
     apps.forEach((app) => {
         app.onclick = (e) => onStarClick(app, e);
 
-        if (configuration.unfocusAppReorderEnabled && app.className.includes('fa-star-o')) {
+        if (configuration.unfocusAppReorderEnabled) {
             let parent = app.parentElement.parentElement;
             // 16.4+ introduce draggable feature, a new parent has been added
             if (parent.parentElement.classList.contains('o_draggable')) {
                 parent = parent.parentElement;
             }
-            container.appendChild(parent);
+
+            const state = app.getAttribute('data-qol-state');
+
+            switch (state) {
+                case `${UNFOCUS_STATE.DEFAULT}`: {
+                    // MAYBE defaultApps.push(parent);
+                    break;
+                }
+                case `${UNFOCUS_STATE.UNFOCUS}`: {
+                    unfocusApps.push(parent);
+                    break;
+                }
+            }
         }
     });
+    defaultApps.forEach((p) => container.appendChild(p));
+    unfocusApps.forEach((p) => container.appendChild(p));
 }
 
+let clickCount = 0;
 async function onStarClick(element, event) {
     event.preventDefault();
     event.stopPropagation();
     event.stopImmediatePropagation();
+
+    // debounce
+    clickCount += 1;
+    currentClickCount = clickCount;
+    await new Promise((r) => setTimeout(r, 200));
+    if (clickCount != currentClickCount || clickCount === 0) return;
 
     let origin = window.location.origin;
     const parent = event.target.parentElement.parentElement;
@@ -87,23 +129,67 @@ async function onStarClick(element, event) {
 
     unfocusAppOrigins[origin] = unfocusAppOrigins[origin] || {};
 
-    if (unfocusAppOrigins[origin][app]) {
-        unfocusAppOrigins[origin][app] = false;
+    const currentState = unfocusAppOrigins[origin][app];
+    const newState = clickCount > UNFOCUS_STATE.MAX ? UNFOCUS_STATE.MAX : clickCount;
+    if (
+        currentState === true || // true was the previous version logic
+        currentState === UNFOCUS_STATE.UNFOCUS ||
+        (currentState !== newState &&
+            (currentState > UNFOCUS_STATE.UNFOCUS || currentState === undefined)) // undefined === default state(1)
+    ) {
+        unfocusAppOrigins[origin][app] =
+            clickCount > UNFOCUS_STATE.MAX ? UNFOCUS_STATE.MAX : clickCount;
     } else {
-        unfocusAppOrigins[origin][app] = true;
+        unfocusAppOrigins[origin][app] = UNFOCUS_STATE.UNFOCUS;
     }
 
     await chrome.storage.sync.set({ unfocusAppOrigins: unfocusAppOrigins });
 
-    if (unfocusAppOrigins[origin][app]) {
-        parent.style.opacity = UNFOCUS_OPACITY;
-        element.classList.remove(FOCUS_ICON);
-        element.classList.add(UNFOCUS_ICON);
-    } else {
-        parent.style.opacity = FOCUS_OPACITY;
-        element.classList.remove(UNFOCUS_ICON);
-        element.classList.add(FOCUS_ICON);
+    updateAppElement(parent, unfocusAppOrigins[origin][app], element);
+
+    clickCount = 0;
+}
+
+function updateAppElement(element, state, starElement) {
+    const isUnfocus = state === UNFOCUS_STATE.UNFOCUS;
+    if (starElement) {
+        starElement.classList.remove(isUnfocus ? FOCUS_ICON : UNFOCUS_ICON);
+        starElement.classList.add(isUnfocus ? UNFOCUS_ICON : FOCUS_ICON);
     }
+    element.style.opacity = isUnfocus ? UNFOCUS_OPACITY : FOCUS_OPACITY;
+
+    const isSuperfocus = state === UNFOCUS_STATE.SUPER;
+    const parent = element.parentElement.classList.contains('o_draggable')
+        ? element.parentElement
+        : element;
+    parent.style.backgroundImage = isSuperfocus ? `url("${superfocusImageURL}")` : null;
+    parent.style.backgroundSize = isSuperfocus ? 'contain' : null;
+    parent.style.backgroundRepeat = isSuperfocus ? 'no-repeat' : null;
+    parent.style.backgroundPosition = 'center top';
+}
+
+async function getThemeModeCookie(origin) {
+    if (!origin.startsWith('http')) return 'light';
+
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieName = decodedCookie.includes('configured_color_scheme')
+        ? 'configured_color_scheme'
+        : decodedCookie.includes('color_scheme')
+        ? 'color_scheme'
+        : null;
+
+    if (!cookieName) return 'light';
+
+    let cookies = decodedCookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length + 1, cookie.length);
+        }
+    }
+
+    return 'light';
 }
 
 chrome.runtime.onMessage.addListener((msg) => {
--- options/src/unfocus_app.js
+++ options/src/unfocus_app.js
@@ -27,6 +27,8 @@ export async function load() {
 export async function restore() {
     const origins = await readUnfocusAppOrigins();
 
+    await restoreImage();
+
     await renderOriginsObject(origins);
 }
 //#endregion
@@ -38,13 +40,19 @@ async function renderOriginsObject(origins) {
     const container = document.getElementById('qol_unfocus_app_table_body');
     container.innerHTML = '';
     Object.keys(origins).forEach((o, id) => {
+        const values = Object.values(origins[o]);
         container.appendChild(
-            renderOrigin(id, o, Object.values(origins[o]).filter((v) => v).length)
+            renderOrigin(
+                id,
+                o,
+                values.filter((v) => v === 2).length,
+                values.filter((v) => v === true || v === 0).length
+            )
         );
     });
 }
 
-function renderOrigin(idx, origin, unfocusCount) {
+function renderOrigin(idx, origin, superfocusCount, unfocusCount) {
     const originTemplate = document.createElement('template');
 
     originTemplate.innerHTML = `
@@ -58,7 +66,8 @@ function renderOrigin(idx, origin, unfocusCount) {
 					value="${origin}"
 				/>
 			</td>
-			<td>${unfocusCount}</td>
+			<td>${superfocusCount}</td>
+            <td>${unfocusCount}</td>
 			<td class="p-1 qol-valign-middle">
 				<button
 					class="qol_unfocus_app_origin_delete_${idx} btn btn-outline-danger border-0 btn-floating"
@@ -78,5 +87,63 @@ function renderOrigin(idx, origin, unfocusCount) {
     deleteButton.onclick = (e) => deleteUnfocusAppOrigin(origin);
 
     return originElement;
+}
+//#endregion
+
+//#region Image
+async function restoreImage() {
+    const { unfocusAppLightImageURL, unfocusAppDarkImageURL } = await StorageSync.get({
+        unfocusAppLightImageURL: '',
+        unfocusAppDarkImageURL: '',
+    });
+
+    document.getElementById('qol_unfocus_app_light_image').src = unfocusAppLightImageURL;
+    document.getElementById('qol_unfocus_app_dark_image').src = unfocusAppDarkImageURL;
+
+    loadImageInput(
+        'qol_unfocus_app_light_image_input',
+        'qol_unfocus_app_light_image',
+        'unfocusAppLightImageURL',
+        unfocusAppLightImageURL
+    );
+
+    loadImageInput(
+        'qol_unfocus_app_dark_image_input',
+        'qol_unfocus_app_dark_image',
+        'unfocusAppDarkImageURL',
+        unfocusAppDarkImageURL
+    );
+}
+
+function loadImageInput(inputKey, imageKey, configKey, value) {
+    const imageInput = document.getElementById(inputKey);
+    imageInput.value = value;
+
+    imageInput.oninput = async (e) => {
+        let imageUrl = e.target.value.trim().replace(/\s/g, '');
+        if (imageUrl.length === 0) {
+            await StorageSync.set({ [configKey]: '' });
+            return;
+        }
+
+        try {
+            imageUrl = new URL(imageUrl);
+            if (!isImageUrlPath(imageUrl.pathname))
+                throw new Error('Invalid image extension in the url path');
+
+            await StorageSync.set({ [configKey]: `${imageUrl}` });
+            imageInput.value = imageUrl;
+            document.getElementById(imageKey).src = imageUrl;
+        } catch (ex) {
+            console.warn(ex);
+        }
+    };
+}
+
+function isImageUrlPath(path) {
+    const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg']; // Add more extensions if needed
+    const lowercaseUrl = path.toLowerCase();
+
+    return imageExtensions.some((extension) => lowercaseUrl.endsWith(extension));
 }
 //#endregion
