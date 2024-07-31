70a920ee2ca7dc1b3f6a678a1717c1bf22ee99cb
Gaetan Vanden Bergh (gavb) <gavb@odoo.com>
2024-07-31 14:10:38 +0200

    [IMP] Review ChatGPT description

--- gh-page/configuration.js
+++ gh-page/configuration.js
@@ -12,10 +12,9 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Instant Assignment:</strong> Quickly assign tasks to yourself with a single click, eliminating delays in task allocation.</li>
-                <li><strong>User Empowerment:</strong> Take charge of your responsibilities, fostering a sense of ownership and accountability.</li>
-                <li><strong>Streamlined Workflow:</strong> Integrates seamlessly with the task management system, promoting efficiency and productivity.</li>
-                <li><strong>Enhanced Collaboration:</strong> Allows team members to proactively manage their workloads, improving overall team dynamics.</li>
+                <li class="pb-3"><strong>Nostalgia:</strong> Before Odoo 16.3, Odoo had a standard button to assign you to a task/ticket. As this button disappear, it's time to add it back!</li>
+                <li class="pb-3"><strong>Page reloaded:</strong> As not standard the button will trigger a reload of the page, that can lead to navigation history lose.</li>
+                <li class="pb-3"><strong>Developer option:</strong> Instead of page reload, choose to append simulated UI.</li>
             </ul>
         </div>`,
         amico: 'add_tasks-amico',
@@ -32,10 +31,7 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Easy Access:</strong> Save articles with a single click, making it simple to keep important information at your fingertips.</li>
-                <li><strong>Organized Research:</strong> Keep your saved articles well-organized, enhancing your research and reference capabilities.</li>
-                <li><strong>Offline Availability:</strong> Ensure that saved articles are available even without an internet connection.</li>
-                <li><strong>Enhanced Productivity:</strong> Spend less time searching for articles and more time utilizing them effectively.</li>
+                <li class="pb-3"><strong>Odoo WIP:</strong> Between the knowledge app addition and Odoo 17.0, there was not an easy way to force the save of an article.</li>
             </ul>
         </div>`,
         amico: 'floppy_disk-amico',
@@ -54,10 +50,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Focus Enhancement:</strong> Hide less-used apps to concentrate on the most important tools for your tasks.</li>
-                <li><strong>Customizable Workspace:</strong> Easily toggle visibility of apps to create a personalized and efficient workspace.</li>
-                <li><strong>Visual Clarity:</strong> Reduce clutter on your Odoo home page, making it easier to find and use essential apps.</li>
-                <li><strong>Highlight Key Apps:</strong> Double-click the star icon to visually emphasize the most critical apps with a background image.</li>
+                <li class="pb-3"><strong>Focus Enhancement:</strong> Hide less-used apps to concentrate on the most important tools for your tasks.</li>
+                <li class="pb-3"><strong>Customizable Workspace:</strong> Easily toggle visibility of apps to create a personalized and efficient workspace.</li>
+                <li class="pb-3"><strong>Visual Clarity:</strong> Reduce clutter on your Odoo home page, making it easier to find and use essential apps.</li>
+                <li class="pb-3"><strong>Highlight Key Apps:</strong> Double-click the star icon to visually emphasize the most critical apps with a background image.</li>
             </ul>
         </div>`,
         amico: 'social_strategy-amico',
@@ -73,10 +69,9 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Automated Setup:</strong> Automatically selects "Execute code" for server actions, reducing setup time.</li>
-                <li><strong>Developer Friendly:</strong> Allows developers to focus directly on writing and testing code without additional steps.</li>
-                <li><strong>Consistency:</strong> Ensures consistent setup for server actions, minimizing errors and improving reliability.</li>
-                <li><strong>Efficiency Boost:</strong> Streamlines the coding process, making development and debugging faster and easier.</li>
+                <li class="pb-3"><strong>Automated Setup:</strong> Automatically selects "Execute code" for server actions, reducing setup time.</li>
+                <li class="pb-3"><strong>Developer Friendly:</strong> Allows developers to focus directly on writing and testing code without additional steps.</li>
+                <li class="pb-3"><strong>Efficiency Boost:</strong> Streamlines the coding process, making development and debugging faster and easier.</li>
             </ul>
         </div>`,
         amico: 'hand_coding-amico',
@@ -91,10 +86,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Instant Information:</strong> View metadata quickly by hovering over the debug icon, saving time on data retrieval.</li>
-                <li><strong>Improved Debugging:</strong> Access important metadata without navigating away from the current view, streamlining the debugging process.</li>
-                <li><strong>Enhanced Usability:</strong> Provides a seamless way to gather necessary information for record analysis and troubleshooting.</li>
-                <li><strong>Time-Saving:</strong> Reduces the need for multiple clicks and page loads, improving overall efficiency.</li>
+                <li class="pb-3"><strong>Instant Information:</strong> View metadata quickly by hovering over the debug icon, saving time on data retrieval.</li>
+                <li class="pb-3"><strong>Improved Debugging:</strong> Access important metadata without navigating away from the current view, streamlining the debugging process.</li>
+                <li class="pb-3"><strong>Enhanced Usability:</strong> Provides a seamless way to gather necessary information for record analysis and troubleshooting.</li>
+                <li class="pb-3"><strong>Time-Saving:</strong> Reduces the need for multiple clicks and page loads, improving overall efficiency.</li>
             </ul>
         </div>`,
         amico: 'static_assets-amico',
@@ -109,10 +104,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Convenient Access:</strong> Adds Odoo's menus to the browser context menu for quick and easy access.</li>
-                <li><strong>Streamlined Navigation:</strong> Navigate Odoo functionalities directly from the right-click menu, saving time.</li>
-                <li><strong>User-Friendly:</strong> Enhances the user experience by integrating Odoo menus into familiar browser controls.</li>
-                <li><strong>Efficiency Improvement:</strong> Reduces the number of steps needed to access Odoo features, increasing productivity.</li>
+                <li class="pb-3"><strong>Convenient Access:</strong> Adds Odoo's menus to the browser context menu for quick and easy access.</li>
+                <li class="pb-3"><strong>Streamlined Navigation:</strong> Navigate Odoo functionalities directly from the right-click menu, saving time.</li>
+                <li class="pb-3"><strong>User-Friendly:</strong> Enhances the user experience by integrating Odoo menus into familiar browser controls.</li>
+                <li class="pb-3"><strong>Efficiency Improvement:</strong> Reduces the number of steps needed to access Odoo features, increasing productivity.</li>
             </ul>
         </div>`,
         amico: 'dropdown_menu-amico',
@@ -129,10 +124,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Version Specific:</strong> Open a runbot instance in a specific version quickly by adding a hash parameter to the URL.</li>
-                <li><strong>Admin Access:</strong> Automatically opens the instance as an admin user in debug mode for enhanced testing and debugging.</li>
-                <li><strong>URL Customization:</strong> Easily customize the URL to access different runbot versions as needed.</li>
-                <li><strong>Time-Saving:</strong> Streamlines the process of opening and configuring runbot instances, improving workflow efficiency.</li>
+                <li class="pb-3"><strong>Version Specific:</strong> Open a runbot instance in a specific version quickly by adding a parameter to the URL.</li>
+                <li class="pb-3"><strong>Admin Access:</strong> Automatically opens the instance as an admin user in debug mode for enhanced testing and debugging.</li>
+                <li class="pb-3"><strong>URL Customization:</strong> Easily customize the URL to access different runbot versions as needed.</li>
+                <li class="pb-3"><strong>Time-Saving:</strong> Streamlines the process of opening and configuring runbot instances, improving workflow efficiency.</li>
             </ul>
         </div>`,
         amico: 'product_tour-amico',
@@ -148,10 +143,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Testing Flexibility:</strong> Easily impersonate default users like admin, demo, or portal for comprehensive testing.</li>
-                <li><strong>Support Enhancement:</strong> Provide better support by experiencing the system from different user perspectives.</li>
-                <li><strong>Quick Switching:</strong> Add options to the login page menu to seamlessly switch between different user roles.</li>
-                <li><strong>Debugging Aid:</strong> Simplifies the process of identifying and resolving user-specific issues.</li>
+                <li class="pb-3"><strong>Testing Flexibility:</strong> Easily impersonate default users like admin, demo, or portal for comprehensive testing.</li>
+                <li class="pb-3"><strong>Support Enhancement:</strong> Provide better support by experiencing the system from different user perspectives.</li>
+                <li class="pb-3"><strong>Quick Switching:</strong> Add options to the login page menu to seamlessly switch between different user roles.</li>
+                <li class="pb-3"><strong>Debugging Aid:</strong> Simplifies the process of identifying and resolving user-specific issues.</li>
             </ul>
         </div>`,
         amico: 'add_user-amico',
@@ -167,10 +162,9 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Instant Access:</strong> Quickly open a runbot instance from the runbot page as an admin user in debug mode.</li>
-                <li><strong>Simplified Debugging:</strong> Adds a new icon next to the database name for easy access to the debug mode.</li>
-                <li><strong>Enhanced Control:</strong> Provides administrators with immediate access to necessary debugging tools.</li>
-                <li><strong>Efficiency Boost:</strong> Reduces the steps needed to access and debug runbot instances, saving time.</li>
+                <li class="pb-3"><strong>Instant Access:</strong> Quickly open a runbot instance from the runbot page as an admin user in debug mode.</li>
+                <li class="pb-3"><strong>Simplified Debugging:</strong> Adds a new icon next to the database name for easy access to the debug mode.</li>
+                <li class="pb-3"><strong>Efficiency Boost:</strong> Reduces the steps needed to access and debug runbot instances, saving time.</li>
             </ul>
         </div>`,
         amico: 'superhero-amico',
@@ -186,10 +180,7 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Dynamic Themes:</strong> Switch Odoo themes based on location, time range, or user preference.</li>
-                <li><strong>Enhanced Customization:</strong> Personalize the Odoo interface to match different work environments or times of day.</li>
-                <li><strong>User Experience:</strong> Improves the visual and functional experience by providing adaptable theme options.</li>
-                <li><strong>Easy Configuration:</strong> Simple to configure and switch themes, making customization accessible to all users.</li>
+                <li class="pb-3"><strong>Dynamic Themes:</strong> Switch Odoo themes based on location, time range, or user preference.</li>
             </ul>
         </div>`,
         amico: 'google_sitemap-amico',
@@ -205,10 +196,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Customization:</strong> Apply custom CSS styles to personalize the appearance of your Odoo database page.</li>
-                <li><strong>Visual Enhancement:</strong> Improve the aesthetics of your Odoo interface to suit your preferences.</li>
-                <li><strong>User-Friendly:</strong> Easily implement CSS changes without needing advanced technical skills.</li>
-                <li><strong>Enhanced Interface:</strong> Make your Odoo experience more engaging and visually appealing.</li>
+                <li class="pb-3"><strong>Customization:</strong> Apply custom CSS styles to personalize the appearance of your Odoo database page.</li>
+                <li class="pb-3"><strong>Visual Enhancement:</strong> Improve the aesthetics of your Odoo interface to suit your preferences.</li>
+                <li class="pb-3"><strong>User-Friendly:</strong> Easily implement CSS changes without needing advanced technical skills.</li>
+                <li class="pb-3"><strong>Enhanced Interface:</strong> Make your Odoo experience more engaging and visually appealing.</li>
             </ul>
         </div>`,
         amico: 'design_process-amico',
@@ -225,10 +216,9 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Loading Design Options:</strong> Choose between a large, fullscreen loading design or a small, corner text.</li>
-                <li><strong>Visual Appeal:</strong> Customize the loading indicator to enhance the visual experience during load times.</li>
-                <li><strong>User Engagement:</strong> Keeps users informed and engaged while waiting for pages to load.</li>
-                <li><strong>Easy Switching:</strong> Quickly switch between different loading designs to suit your needs.</li>
+                <li class="pb-3"><strong>Visual Appeal:</strong> Customize the loading indicator to enhance the visual experience during load times.</li>
+                <li class="pb-3"><strong>User Engagement:</strong> Keeps users informed and engaged while waiting for pages to load.</li>
+                <li class="pb-3"><strong>Easy Switching:</strong> Quickly switch between different loading designs to suit your needs.</li>
             </ul>
         </div>`,
         amico: 'progress_indicator-amico',
@@ -244,10 +234,10 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Visual Feedback:</strong> Adds a visually pleasing stars particle effect when prioritizing tasks.</li>
-                <li><strong>Engaging Experience:</strong> Makes task prioritization more enjoyable and interactive.</li>
-                <li><strong>Instant Recognition:</strong> Provides immediate visual confirmation when a task is prioritized.</li>
-                <li><strong>Motivation Boost:</strong> Enhances user satisfaction and motivation through engaging visuals.</li>
+                <li class="pb-3"><strong>Visual Feedback:</strong> Adds a visually pleasing stars particle effect when prioritizing tasks.</li>
+                <li class="pb-3"><strong>Engaging Experience:</strong> Makes task prioritization more enjoyable and interactive.</li>
+                <li class="pb-3"><strong>Instant Recognition:</strong> Provides immediate visual confirmation when a task is prioritized.</li>
+                <li class="pb-3"><strong>Motivation Boost:</strong> Enhances user satisfaction and motivation through engaging visuals.</li>
             </ul>
         </div>`,
         amico: 'launching_with_balloons-amico',
@@ -262,9 +252,9 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Recognition:</strong> Displays user badges in the user card, highlighting achievements and credentials.</li>
-                <li><strong>Enhanced Profiles:</strong> Makes user profiles more informative and visually appealing.</li>
-                <li><strong>Community Building:</strong> Promotes a sense of achievement.</li>
+                <li class="pb-3"><strong>Recognition:</strong> Displays user badges in the user card, highlighting achievements and rewards.</li>
+                <li class="pb-3"><strong>Enhanced Profiles:</strong> Makes user profiles more informative and visually appealing.</li>
+                <li class="pb-3"><strong>Community Building:</strong> Promotes a sense of achievement.</li>
             </ul>
         </div>`,
         amico: 'brand_loyalty-amico',
@@ -280,9 +270,9 @@ export const features = [
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li><strong>Priority Access:</strong> Ensures important messages are always at the top of the chat, preventing the from getting lost in the conversation flow.</li>
-                <li><strong>Easy management:</strong> Simple pin and unpin actions allow for dynamic management of important messages.</li>
-                <li><strong>Improved communication:</strong> Reduces the need to scroll through long chat histories to find critical messages, enhancing overall communication efficiency.</li>
+                <li class="pb-3"><strong>Priority Access:</strong> Ensures important messages are always at the top of the chat, preventing the from getting lost in the conversation flow.</li>
+                <li class="pb-3"><strong>Easy management:</strong> Simple pin and unpin actions allow for dynamic management of important messages.</li>
+                <li class="pb-3"><strong>Improved communication:</strong> Reduces the need to scroll through long chat histories to find critical messages, enhancing overall communication efficiency.</li>
             </ul>
         </div>`,
         amico: 'my_answer-amico',
