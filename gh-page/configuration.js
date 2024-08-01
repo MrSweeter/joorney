5a39eebbb2aae32996db6e35b6ca187ce139b3dd
Gaetan Vanden Bergh (gavb) <gavb@odoo.com>
2024-08-01 12:32:34 +0200

    [IMP] Review ChatGPT description

--- gh-page/configuration.js
+++ gh-page/configuration.js
@@ -1,4 +1,4 @@
-// TODO[MANDATORY] CHECK additionalDescription
+// TODO[IMP] additionalDescription + video
 
 export const features = [
     {
@@ -27,7 +27,7 @@ export const features = [
             '<em>Save Article</em> simplifies content management by adding a save button to the top right corner of article pages. This button ensures that you can easily save important articles for later reference with just one click. This feature is perfect for users who frequently research or need to keep track of various articles and information.',
         textDescription:
             'Add a new button on the top right corner of the article page that you can click to force the save of it.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -46,7 +46,7 @@ export const features = [
             '<em>Unfocus Apps</em> helps you maintain productivity by allowing you to hide apps that are not frequently used. By clicking the star next to the app name on the Odoo home page, you can toggle the visibility of these apps, focusing on those more relevant to your work. Additionally, you can highlight an app by double-clicking the star to add a background image around the app icon.',
         textDescription:
             'Hide the apps that you are not using frequently, so you can focus on the ones that are more relevant to your work.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -65,7 +65,7 @@ export const features = [
         longDescription:
             '<em>Server Action Code</em> optimizes your workflow by automatically selecting "Execute code" as the type for Server Action. This allows you to focus directly on writing code without manually setting the action type, making the coding process more efficient and streamlined.',
         textDescription: 'Automatically select "Execute code" as type for Server Action',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -82,7 +82,7 @@ export const features = [
         title: 'Tooltip Metadata',
         longDescription: `<em>Tooltip Metadata</em> provides quick access to record metadata. By hovering over the debug icon, you can instantly view the current record's metadata, making it easier to gather essential information without navigating away from the current view.`,
         textDescription: 'Show you current record metadata by hovering the debug icon.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -100,7 +100,7 @@ export const features = [
         title: 'Context OdooMenus',
         longDescription: `<em>Context OdooMenus</em> integrates Odoo's menus into the browser context menu. This addition allows you to access Odoo's functionalities directly from the browser's right-click menu, providing a more seamless and integrated user experience.`,
         textDescription: "Add Odoo's menus to the browser context menu",
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -120,7 +120,7 @@ export const features = [
             '<em>Auto-Open Runbot</em> facilitates quick access to specific runbot instances. By adding a search parameter to the URL, you can open a runbot instance in a specific version as an admin user in debug mode. For example, accessing a runbot in version 17.0 can be done through a URL like <a target="_blank" href="https://runbot.odoo.com?joorney-runbot=17.0">https://runbot.odoo.com?joorney-runbot=17.0</a>.',
         textDescription:
             'Open a runbot instance in a specific version: <a target="_blank" href="https://runbot.odoo.com?joorney-runbot=17.0">https://runbot.odoo.com?joorney-runbot=17.0</a>',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -139,7 +139,7 @@ export const features = [
         longDescription:
             '<em>Impersonate User</em> allows you to test and support effectively by impersonating default users on a runbot, such as admin, demo, or portal. This feature adds options to the login page menu, enabling you to select and impersonate different default users for various purposes.',
         textDescription: 'Impersonate a default user on a runbot, such as admin, demo, or portal.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -158,7 +158,7 @@ export const features = [
         longDescription:
             '<em>Admin-Debug Login</em> provides an easy way to open a runbot instance from the runbot page as an admin user in debug mode. This feature adds a new icon next to the database name, allowing you to access the runbot instance with a single click, facilitating easier debugging and developer tasks.',
         textDescription: 'Open a runbot instance from the runbot page as an admin user in debug mode.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -176,7 +176,7 @@ export const features = [
         longDescription:
             '<em>Switch Odoo Theme</em> allows you to dynamically change the Odoo theme based on configured options. You can switch themes depending on location, time range, or simply choose a preferred one, enhancing the visual and functional experience of using Odoo.',
         textDescription: 'Switch the Odoo theme dynamically based on a configured option',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -192,7 +192,7 @@ export const features = [
         longDescription:
             '<em>Awesome CSS</em> gives you the power to customize the appearance of your Odoo database page by applying custom CSS styles. This feature is perfect for users who want to personalize and enhance the visual aspects of their Odoo interface.',
         textDescription: 'Apply custom CSS styles on your Odoo database page.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -212,7 +212,7 @@ export const features = [
         longDescription:
             '<em>Awesome Loading</em> allows you to customize the loading design of Odoo. You can choose between a large loading design, which displays a spinning circle in fullscreen, or a small one, which shows a "Loading" text at the bottom right of the page. This feature enhances the user experience during load times.<br /><br /><i>Note that the large loading has been removed in Odoo 17.0</i>',
         textDescription: 'Change the loading design of Odoo, the large one or the small one.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -230,13 +230,13 @@ export const features = [
         longDescription:
             '<em>Stars Effect</em> adds a touch of magic to task prioritization. When you prioritize a task, it triggers a stars particles effect, adding a visually pleasing element to task management and making the prioritization process more engaging.',
         textDescription: 'Trigger stars particles effect when you prioritize a task.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
-                <li class="pb-3"><strong>Visual Feedback:</strong> Adds a visually pleasing stars particle effect when prioritizing tasks.</li>
-                <li class="pb-3"><strong>Engaging Experience:</strong> Makes task prioritization more enjoyable and interactive.</li>
-                <li class="pb-3"><strong>Instant Recognition:</strong> Provides immediate visual confirmation when a task is prioritized.</li>
+                <li class="pb-3"><strong>Visual Feedback:</strong> Adds a visually pleasing stars particle effect when prioritizing tasks/tickets.</li>
+                <li class="pb-3"><strong>Engaging Experience:</strong> Makes task/ticket prioritization more enjoyable and interactive.</li>
+                <li class="pb-3"><strong>Instant Recognition:</strong> Provides immediate visual confirmation when a task/ticket is prioritized.</li>
                 <li class="pb-3"><strong>Motivation Boost:</strong> Enhances user satisfaction and motivation through engaging visuals.</li>
             </ul>
         </div>`,
@@ -248,7 +248,7 @@ export const features = [
         title: 'Show my Badge',
         longDescription: `<em>Show My Badge</em> displays user badges in the user card, highlighting achievements. This feature is ideal for recognizing and showcasing users' accomplishments within the Odoo interface, promoting a sense of achievement and community.`,
         textDescription: "Show user's badges in the user card.",
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
@@ -266,7 +266,7 @@ export const features = [
         longDescription:
             '<em>Pin Message</em> allows users to quickly pin important messages of the chatter. By pinning a message, you ensure that critical information is easily accessible. Enhance communication efficiency by keeping key message highlighted and easy to find.',
         textDescription: 'Pin message to the top of chatter fo easy access.',
-        video: './assets/video/2715412-hd_1280_720_60fps.webm',
+        video: '',
         additionalDescription: `<div class="additional-info">
             <p class="fw-bold">Additional Information:</p>
             <ul>
